8805807d893095c4127dd3b57897ac868122adb9
ny <mac@nythepegas.us>
2025-06-16 04:54:15 -0400

    fix(tests): test current language, an attribute that potentially changes

--- Tests/MacAnisetteTests/MacAnisetteTests.swift
+++ Tests/MacAnisetteTests/MacAnisetteTests.swift
@@ -3,5 +3,6 @@ import Testing
 
 @Test func getanisette() async throws {
     let ani = getAOSAnisette()
+    #expect(ani.locale.language == NSLocale.current.language)
     #expect(ani.routingInfo == 0)
 }
