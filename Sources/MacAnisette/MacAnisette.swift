56eed89dee230a9b510fc91e3f6646d86fd01129
Stossy11 <69031796+stossy11@users.noreply.github.com>
2025-06-21 12:09:43 +1000

    Remove StosSign and use a dictionary

--- Sources/MacAnisette/MacAnisette.swift
+++ Sources/MacAnisette/MacAnisette.swift
@@ -1,14 +1,7 @@
 @_exported import AOSKit
 import Foundation
-import StosSign
 
 /// Uses `AOSKit` to return `AnisetteData`
 @available(macOS 10.13, *)
-public func getAOSAnisette() -> AnisetteData {
-    let dict = AOSKit.getAnisetteData()
-    // Convert NSDictionary to JSON Data
-    let jsonData = try! JSONSerialization.data(withJSONObject: dict ?? [], options: [])
-    
-    return try! JSONDecoder().decode(AnisetteData.self, from: jsonData)
-}
+public func getAOSAnisette() -> [AnyHashable : Any] { AOSKit.getAnisetteData() ?? [:] }
 
